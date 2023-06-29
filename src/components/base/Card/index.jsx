1e9f689678f260b339bb360971e95e40d8282ebe
My First Robot Clothing Consultant <aryairama987@gmail.com>
2023-06-29 15:36:19 +0700

    update : page home 70% progress

---
+++ src/components/base/Card/index.jsx
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import { Card as CardFlowbite } from 'flowbite-react';
+import style from './style.module.css';
+
+const Card = ({ className, children, theme }) => {
+  return (
+    <CardFlowbite theme={theme} className={`w-full ${className}`}>
+      {children}
+    </CardFlowbite>
+  );
+};
+
+Card.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+  theme: PropTypes.object,
+};
+
+Card.defaultProps = {
+  className: '',
+  theme: {},
+};
+
+Card.Divider = () => {
+  return <hr className={style['card-divider']} />;
+};
+
+Card.Divider.displayName = 'Card.Divider';
+export default Card;
+
+export const CardThemeWithoutGap = {
+  root: {
+    base: 'flex rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800',
+    children: 'flex h-full flex-col justify-center p-6',
+    horizontal: {
+      off: 'flex-col',
+      on: 'flex-col md:max-w-xl md:flex-row',
+    },
+    href: 'hover:bg-gray-100 dark:hover:bg-gray-700',
+  },
+  img: {
+    base: '',
+    horizontal: {
+      off: 'rounded-t-lg',
+      on: 'h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg',
+    },
+  },
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,81 +1,59 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
+import { Card } from '../../components/base';
+import style from './style.module.css';
+import { IonIcon } from '@ionic/react';
+import { analyticsOutline, cameraOutline, documentTextOutline, documentOutline, timeOutline } from 'ionicons/icons';
+import { CardThemeWithoutGap } from '../../components/base/Card';
 
 const Home = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ut id eaque, iusto laudantium ex voluptatibus
-      consectetur quibusdam eligendi deserunt. Iusto nisi eos quam deleniti porro consequuntur praesentium deserunt?
-      Tempore fuga, quasi esse velit cumque quod, recusandae debitis libero in asperiores explicabo est non labore alias
-      ipsam. Nostrum, optio veniam ullam iusto velit id. Ab laudantium earum hic veniam quae voluptates neque enim eum
-      omnis id dolor sequi incidunt ullam, iure expedita fugiat placeat qui consequuntur veritatis, nisi ad sint atque.
-      Doloremque ullam inventore, sunt cumque distinctio rerum. Explicabo reiciendis consectetur et nemo architecto
-      magni fugiat expedita, quia beatae temporibus, pariatur rerum repellat, corrupti vitae quas consequatur aliquid
-      nostrum enim iure placeat aspernatur maxime iste error soluta. Ut dolor ducimus id, non sunt aut quae fuga ullam
-      unde, atque quaerat nesciunt debitis necessitatibus tempore sequi eos dolores qui quos maiores voluptatem quod
-      consequatur minima corporis! Veritatis recusandae ullam nobis excepturi iure alias assumenda nemo veniam tenetur,
-      officiis temporibus deserunt pariatur incidunt blanditiis delectus qui beatae magni? Quibusdam praesentium
-      suscipit asperiores vel ipsa saepe, quam odit numquam necessitatibus repellat libero, similique id a ut nemo. Non
-      repudiandae at expedita debitis, asperiores incidunt facilis mollitia dolorem commodi recusandae itaque quidem
-      sunt nulla suscipit doloremque, temporibus omnis, perspiciatis exercitationem soluta culpa dignissimos est
-      blanditiis? Fuga sunt cumque culpa. Ducimus omnis est nulla placeat, ad ipsa explicabo, perferendis molestiae
-      tempora autem dolorem alias exercitationem inventore sequi eveniet cumque consectetur similique quasi pariatur,
-      fugit totam adipisci. Iure dolorum neque fugiat nulla, nesciunt quas, autem nemo repellendus atque rerum dolores
-      voluptatem praesentium repellat asperiores obcaecati magni minus placeat architecto suscipit blanditiis
-      perferendis, fuga commodi. Nulla voluptatem nemo eius totam vitae, quaerat ea in laboriosam quam ipsam et quod
-      officiis, unde a sequi similique voluptate deserunt iste! Commodi, praesentium omnis accusamus neque voluptates
-      qui error nulla in ea, unde veniam ad facere laudantium quisquam! Atque voluptatem facere eligendi ab consequatur
-      error velit, ipsam quia, doloremque cupiditate dolore quod ad distinctio sapiente debitis blanditiis laudantium
-      nemo numquam aliquid possimus. Minima obcaecati pariatur consequuntur distinctio maxime ducimus, labore eius,
-      eveniet sed ut unde numquam quos quam corporis minus molestiae a omnis alias ad in dolor cumque placeat provident
-      velit! Sit, voluptatum minus dignissimos repudiandae officiis veritatis cupiditate. A eum non quis sapiente? Velit
-      fugiat ducimus rem optio qui aperiam laudantium dignissimos minus sint deleniti cupiditate neque cumque tempore
-      molestias, consequuntur sunt sit libero. Ullam eos vel ipsa reiciendis fuga dolor beatae sapiente corrupti
-      veritatis autem consectetur, illum, sit deleniti commodi sunt aliquam distinctio quas? Nihil similique, delectus
-      modi quisquam velit sequi quidem saepe at accusamus quo placeat perferendis consectetur culpa distinctio nemo iure
-      quae voluptatem beatae sed deleniti! Ipsa, quam recusandae eveniet accusantium beatae excepturi deserunt minima
-      ullam quibusdam atque fugit illum quas laboriosam quae exercitationem necessitatibus assumenda aut nesciunt.
-      Soluta exercitationem porro minus quos similique architecto reiciendis consequuntur fuga nulla! Dignissimos nisi
-      molestiae explicabo esse culpa quae, minima sunt suscipit eligendi aliquid sit iste, illo, quisquam aperiam neque
-      optio ea impedit mollitia vero debitis sequi quo quasi quidem doloribus. Provident veritatis in adipisci tempore.
-      Aliquid, impedit sapiente totam, alias sunt enim odit voluptate praesentium laborum incidunt rerum unde! Animi,
-      labore voluptatibus cum distinctio amet laborum consectetur tempora quidem eveniet error tenetur? Molestiae
-      nostrum omnis nesciunt a harum consequatur, obcaecati iusto, accusantium nemo natus odio possimus officiis.
-      Aliquid ullam in suscipit. Temporibus maxime nam voluptates sequi, magnam similique nemo dolorem et assumenda iure
-      cumque repellat sed, nobis totam ea enim at. Omnis explicabo doloremque magnam tenetur? Tempore nisi, commodi
-      voluptates, blanditiis maiores ullam culpa quibusdam accusamus harum laborum inventore a expedita laboriosam odit
-      tempora. Commodi eius, nemo molestiae inventore perferendis aperiam, ut temporibus incidunt ullam nam ipsam
-      tempora modi quo, aspernatur itaque dolorum soluta natus nobis minima illo explicabo. Est et voluptas temporibus
-      eveniet nisi quae dignissimos distinctio cum perspiciatis. Dolorem vitae eaque in placeat. Provident itaque, amet
-      culpa facilis dolor omnis iusto expedita ipsum doloribus saepe quas. Nemo quae, ipsum debitis provident deleniti
-      omnis explicabo tempore delectus reiciendis amet ipsa modi commodi! Voluptates dolore impedit omnis et facere, eum
-      repellendus laudantium asperiores. Deleniti nihil, cupiditate eos officiis unde enim nulla maiores aliquam
-      consequuntur dolorem iure porro laborum debitis in fugit hic. Magni quibusdam porro dolorem. Et ad corrupti
-      deserunt perferendis nostrum eligendi dolor amet adipisci ducimus soluta laborum officia nam, voluptate deleniti.
-      Dolore ut obcaecati distinctio voluptate officia. Voluptate perferendis sint aspernatur, praesentium ipsam
-      exercitationem possimus iusto ut optio cum, neque ipsum, ducimus soluta. Nostrum, asperiores. Culpa beatae
-      possimus incidunt expedita, earum illo error nesciunt reprehenderit, corporis iusto eum quis voluptatum.
-      Obcaecati, minima commodi? Expedita odio nemo impedit quam totam a, eaque eum laboriosam consectetur recusandae,
-      enim reiciendis dolore doloribus ut debitis consequuntur soluta deleniti assumenda voluptates nulla nisi nostrum!
-      Obcaecati ullam ducimus architecto voluptatem minus commodi, ut dolores esse blanditiis possimus corrupti adipisci
-      odit perspiciatis explicabo ipsa quaerat velit laboriosam earum aliquid nostrum cupiditate! Fugiat enim aut minus,
-      ab fugit molestias debitis deleniti modi ad, neque eveniet in quas eligendi id suscipit accusamus dolores, ex
-      voluptate nisi illo corrupti facere iusto sequi dolore. Unde commodi aliquam maxime ab voluptas distinctio aperiam
-      iure iste beatae. Non, magnam odio suscipit iure aut nesciunt. Voluptas voluptates fuga at autem sint vel error
-      nostrum sequi sed quis voluptatibus porro tempore similique placeat officia, reprehenderit repudiandae culpa
-      maiores, voluptatum omnis vero, repellat cumque neque? Libero delectus, consequatur recusandae ad totam odit vero
-      sed ut aut alias, aspernatur tempora assumenda eaque asperiores facilis omnis qui porro! Eius nam provident, in,
-      beatae illum voluptatibus autem voluptates fugiat, accusamus neque esse quisquam commodi corrupti odio
-      necessitatibus itaque quis at incidunt dolorem dolorum inventore dolore ex. Perspiciatis incidunt eligendi in
-      ipsam? Distinctio sapiente consectetur accusantium enim repellendus error doloribus provident dignissimos iure
-      quam, veritatis maiores! Aliquid natus est nam dolore. Error incidunt sequi ut magni quisquam. Consequuntur
-      nesciunt veniam magni necessitatibus assumenda soluta nostrum eum sed. Sint nihil id excepturi est quaerat, enim
-      doloremque. Tenetur ut dolorum, modi asperiores minima magni error illum? Dolores provident tempore perferendis
-      nemo. Doloribus, sapiente. Officia voluptates necessitatibus explicabo id temporibus?
-    </div>
+    <>
+      <Card>
+        <div className={style['greeting-text-layout']}>
+          <div className={style['greeting-text-section']}>
+            <p className={style['greeting-text']}>Selamat Siang</p>
+            <p className={style['greeting-username']}>Arya Irama Wahono</p>
+          </div>
+          <div className={style['greeting-text-section']}>
+            <div className={style['working-hours']}>Jam Kerja</div>
+            <div className={style['working-hours-text']}>06:30:00 - 16:30:00</div>
+          </div>
+        </div>
+        <Card.Divider></Card.Divider>
+        <div className={style['list-menu-layout']}>
+          <div className={style['list-menu']}>
+            <IonIcon className={`${style['list-menu-icon']} bg-[#1b90de]`} icon={cameraOutline}></IonIcon>
+            <p>Absensi</p>
+          </div>
+          <div className={style['list-menu']}>
+            <IonIcon className={`${style['list-menu-icon']} bg-[#FFB400]`} icon={documentOutline}></IonIcon>
+            <p>Izin</p>
+          </div>
+          <div className={style['list-menu']}>
+            <IonIcon className={`${style['list-menu-icon']} bg-[#1b90de]`} icon={timeOutline}></IonIcon>
+            <p>Lembur</p>
+          </div>
+          <div className={style['list-menu']}>
+            <IonIcon className={`${style['list-menu-icon']} bg-[#754AED]`} icon={analyticsOutline}></IonIcon>
+            <p>Laporan Pekerjaan</p>
+          </div>
+          <div className={style['list-menu']}>
+            <IonIcon className={`${style['list-menu-icon']} bg-[#1DCC70]`} icon={documentTextOutline}></IonIcon>
+            <p>HIstory</p>
+          </div>
+        </div>
+      </Card>
+      <div className={style['status-attendance-layout']}>
+        <Card theme={CardThemeWithoutGap} className={`${style['status-attendance-card']} bg-[#1b90de]`}>
+          <p className={style['status-attendance-card-label']}>Absen Masuk</p>
+          <p className={style['status-attendance-card-value']}>Belum Absen</p>
+        </Card>
+        <Card theme={CardThemeWithoutGap} className={`${style['status-attendance-card']} bg-[#8494A8]`}>
+          <p className={style['status-attendance-card-label']}>Absen Pulang</p>
+          <p className={style['status-attendance-card-value']}>Belum Absen</p>
+        </Card>
+      </div>
+    </>
   );
 };
-
-Home.propTypes = {};
 
 export default Home;
