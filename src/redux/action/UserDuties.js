ebe8a5daa59e64e21a0cfa2b79174996035b1a59
My First Robot Clothing Consultant <aryairama987@gmail.com>
2023-07-01 09:10:38 +0700

    update : config vite support js component and listing user duties

--- src/redux/action/UserDuties.js
+++ src/redux/action/UserDuties.js
@@ -5,8 +5,10 @@ import { toast } from 'react-toastify';
 import axios from '../../config/axios';
 
 export const userDutiesAreas = async () => {
+  store.dispatch(SHOW_LOADER());
   try {
     const { data } = await (await axios().get(`/user-duties/areas`)).data;
+    store.dispatch(HIDDEN_LOADER());
     return data;
   } catch (error) {
     if (error.response.status >= 400 && error.response.status < 500) {
@@ -15,6 +17,7 @@ export const userDutiesAreas = async () => {
       toast.error('Terjadi kesalahan, silahkan coba lagi!');
     }
   }
+  store.dispatch(HIDDEN_LOADER());
 };
 
 export const userDutiesAreasFilter = async (id) => {
@@ -28,4 +31,26 @@ export const userDutiesAreasFilter = async (id) => {
       toast.error('Terjadi kesalahan, silahkan coba lagi!');
     }
   }
+};
+
+export const userDutiesChecklist = async (ids = [], file, setReload) => {
+  store.dispatch(SHOW_LOADER());
+  try {
+    const batchFetch = [];
+    ids.forEach((value) => {
+      const formData = new FormData();
+      formData.append('user_duty_id', value);
+      batchFetch.push(axios().post(`/user-duties/checklist`, formData));
+    });
+    await Promise.all(batchFetch);
+    setReload((oldVal) => !oldVal);
+    toast.success('Berhasil melakukan pekerjaan');
+  } catch (error) {
+    if (error.response.status >= 400 && error.response.status < 500) {
+      toast.warn(error.response.data?.data?.message || error.response?.data?.message);
+    } else {
+      toast.error('Terjadi kesalahan, silahkan coba lagi!');
+    }
+  }
+  store.dispatch(HIDDEN_LOADER());
 };
